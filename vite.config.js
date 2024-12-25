8dab4cc2bcedb822d4ca7a18177e965f58a29925
felix toledo <felixtoledoctes@gmail.com>
2024-12-25 02:25:39 -0300

    changed favicon and icon from cellphones pwa

--- vite.config.js
+++ vite.config.js
@@ -16,17 +16,17 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/sierrast_logo.png',
+            src: '/ski_tech_sierra_logo.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/sierrast_logo.png',
+            src: '/ski_tech_sierra_logo.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/sierrast_logo.png',
+            src: '/ski_tech_sierra_logo.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
